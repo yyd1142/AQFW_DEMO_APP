f91b231bd4cf69603fc1d5b96117a8c8833c64b3
Queue <tantan313@126.com>
2017-11-06 19:57:15 +0800

    监测模块UI

---
+++ src/views/monitorDq/DeviceList.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="device-list-wrap">
+        <div class="title-info">
+            <div class="label fl">设备列表</div>
+            <div class="value fr">共1个设备</div>
+        </div>
+        <mko-double-cell title="测温式电气火灾监控探测器"
+                         label="深圳湾体育中心-体育馆，-1层，东北角巡查点" is-link>
+            正常
+        </mko-double-cell>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {}
+        },
+        watch: {},
+        computed: {},
+        mounted() {
+        },
+        activated(){
+        },
+        deactivated() {
+        },
+        destroyed(){
+        },
+        methods: {},
+        components: {}
+    }
+</script>
+
+<style lang="less" rel="stylesheet/less">
+    @import "../../config.less";
+</style>
--- src/views/monitorDq/index.vue
+++ src/views/monitorDq/index.vue
@@ -2,15 +2,28 @@
     <div>
         <mko-header title="电气监测" left-icon="icon-back" @handleLeftClick="back"></mko-header>
         <div class="page-wrap monitor-dq-wrap">
-
+            <mko-nav-bar>
+                <mko-tab-item :label="item.text" :activied="tabI==i" @handleTabClick="tab(i)"
+                              v-for="(item,i) in headerBtnItems"></mko-tab-item>
+            </mko-nav-bar>
+            <alarm-list v-show="tabI==0"></alarm-list>
+            <device-list v-show="tabI==1"></device-list>
         </div>
     </div>
 </template>
 
 <script>
+    import alarmList from './AlarmList.vue'
+    import deviceList from './DeviceList.vue'
     export default {
         data () {
-            return {}
+            return {
+                tabI: 0,
+                headerBtnItems: [
+                    {text: '设备报警'},
+                    {text: '电气设备'},
+                ]
+            }
         },
         watch: {},
         computed: {},
@@ -23,11 +36,16 @@
         destroyed(){
         },
         methods: {
+            tab(i){
+                this.tabI = i;
+            },
             back(){
                 this.$MKOPop();
             }
         },
-        components: {}
+        components: {
+            alarmList,deviceList
+        }
     }
 </script>
 
@@ -35,6 +53,19 @@
     @import "../../config.less";
 
     .monitor-dq-wrap {
-
+        .title-info {
+            height: 34px;
+            padding: 14px 14px 8px;
+            line-height: 12px;
+            font-size: 12px;
+            letter-spacing: 0;
+            background-color: @baseBG01;
+            .label {
+                color: @baseText02;
+            }
+            .value {
+                color: @baseText03;
+            }
+        }
     }
 </style>
